d990967b8b5e83104f676f57fdd5a7f1722899d2
Haytham Mohamed <haybu@hotmail.com>
2019-02-02 16:25:50 -0600

    registry-server

--- cards-client/src/main/java/io/agilehandy/client/MainApplication.java
+++ cards-client/src/main/java/io/agilehandy/client/MainApplication.java
@@ -3,6 +3,7 @@ package io.agilehandy.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,7 @@ public class MainApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplateBuilder().build();
 	}
